ae58a570907efa7a3c0bf048463b436ea6e995f9
uekitateiji <uekita.teiji@gmail.com>
2025-03-02 00:50:21 -0300

    feat: refactoring ProductCardxc

--- src/shared/components/ProductCard/index.tsx
+++ src/shared/components/ProductCard/index.tsx
@@ -18,52 +18,36 @@ const CardProduct = ({
       ${isCompact ? "max-w-64 h-72 mr-2" : "w-full flex-row h-56 mb-2"}
       ${isHighlight ? "border-purple-500" : "border-gray-200"}
     `}>
-      <ImageBackground source={productImage} className={`flex-[2] ${isCompact ? "w-full rounded-t-lg" : "h-full"}`} />
-      <ProductDetails
-        title={title}
-        price={price}
-        advertiser={advertiser}
-        rating={rating}
-        reviews={reviews}
-        isHighlight={isHighlight}
-      />
+      <ImageBackground source={productImage} className={`flex-[2] ${isCompact ? "w-full rounded-t-lg" : "h-full"}`} >
+
+
+        {isHighlight &&
+          <View className="absolute bottom-0  bg-purple-500 px-2 py-1">
+            <Text className="text-white font-medium">DESTAQUE</Text>
+          </View>}
+      </ImageBackground>
+      <View className="flex-[2] p-2 justify-between">
+        {/* {isHighlight && <HighlightTag />} */}
+        <View>
+          <Text className="font-semibold text-2xl">R$ {price}</Text>
+          <Text>{title}</Text>
+        </View>
+        <View className="flex-col">
+          <View className="flex-row items-center">
+            <Ionicons name="person" size={16} color="#000" className="pr-1" />
+            <Text className="text-lg">{advertiser}</Text>
+          </View>
+
+          <View className="flex-row items-center">
+            <Ionicons name="star" size={16} color="#FACC15" />
+            <Text className="font-semibold text-primary pr-1">{rating.toFixed(1)}</Text>
+            <Text className="text-gray-500 text-sm">({reviews} avaliações)</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
 
-
-const ProductDetails = ({
-  title,
-  price,
-  advertiser,
-  rating,
-  reviews,
-  isHighlight
-}: Omit<ProductCardProps, "">) => (
-  <View className="flex-[2] p-2 justify-between">
-    {isHighlight && <HighlightTag />}
-    <View>
-      <Text className="font-semibold text-2xl">R$ {price}</Text>
-      <Text>{title}</Text>
-    </View>
-    <View className="flex-col">
-      <View className="flex-row items-center">
-        <Ionicons name="person" size={16} color="#000" className="pr-1" />
-        <Text className="text-lg">{advertiser}</Text>
-      </View>
-
-      <View className="flex-row items-center">
-        <Ionicons name="star" size={16} color="#FACC15" />
-        <Text className="font-semibold text-primary pr-1">{rating.toFixed(1)}</Text>
-        <Text className="text-gray-500 text-sm">({reviews} avaliações)</Text>
-      </View>
-    </View>  </View>
-);
-
-const HighlightTag = () => (
-  <View className="absolute bottom-0 left-[-84px] bg-purple-500 px-2 py-1">
-    <Text className="text-white font-medium">DESTAQUE</Text>
-  </View>
-);
 
 export default CardProduct;
